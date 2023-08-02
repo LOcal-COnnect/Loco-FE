1a56ae5b345902934f8f882469986960f2c6353a
HYERINI <rinlee2001@naver.com>
2023-08-03 01:07:20 +0900

    FEAT: 가게마이페이지 추가

---
+++ js/userMypage.js
@@ -0,0 +1,35 @@
+const tabList = Array.from(document.querySelectorAll('.tab'));
+
+tabList.forEach((button) => {
+    button.addEventListener('click', handleSortButtonClick);
+});
+
+function handleSortButtonClick(event) {
+    const clickedTab = event.target;
+
+    tabList.forEach((button) => {
+        button.classList.remove('active');
+        if (button === clickedTab) {
+            button.classList.add('active');
+        } else {
+            button.classList.remove('active');
+        }
+    });
+
+    var tab1 = document.getElementsByClassName('tab1')[0];
+    var tab2 = document.getElementsByClassName('tab2')[0];
+    var tab3 = document.getElementsByClassName('tab3')[0];
+    if (clickedTab.id === 'tab1') {
+        tab1.style.display = 'block';
+        tab2.style.display = 'none';
+        tab3.style.display = 'none';
+    } else if (clickedTab.id === 'tab2') {
+        tab1.style.display = 'none';
+        tab2.style.display = 'block';
+        tab3.style.display = 'none';
+    } else if (clickedTab.id === 'tab3') {
+        tab1.style.display = 'none';
+        tab2.style.display = 'none';
+        tab3.style.display = 'block';
+    }
+}
