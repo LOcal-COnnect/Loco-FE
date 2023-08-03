edc9e10b74a1361e634f2b68466890f83975deac
HYERINI <rinlee2001@naver.com>
2023-08-03 21:42:06 +0900

    FEAT: 내브바 수정

--- js/nav.js
+++ js/nav.js
@@ -1,68 +1,81 @@
-function search() {
-    var searchText = document.getElementById("searchInput").value;
-    // 여기에 검색 기능을 구현하는 코드를 작성합니다.
-    // 검색할 단어(searchText)를 활용하여 검색 동작을 수행합니다.
-    // 예를 들어, 검색 결과를 보여주거나 검색 페이지로 이동하는 등의 동작을 수행할 수 있습니다.
-    // 이 예제에서는 검색어를 콘솔에 출력하는 것으로 대체합니다.
-    console.log("검색어: " + searchText);
-}
-
-document.getElementById("searchInput").addEventListener("keydown", function (event) {
-    if (event.key === "Enter") {
-        search();
-    }
+// nav.js
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('nav.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('navbar').innerHTML = data;
+            // 내비게이션 바가 로드된 후에 이벤트를 설정합니다.
+            setupNavigationEvents();
+        })
+        .catch(error => {
+            console.error('Error fetching navbar:', error);
+        });
 });
 
-var homeBtn = document.getElementById('homeButton');
-var loginBtn = document.getElementById('loginButton');
-var registerBtn = document.getElementById('registerButton');
+window.addEventListener('load', createNavbar);
 
-homeBtn.addEventListener('click', function () {
-    window.location.href = 'mainpage.html';
-});
-
-loginBtn.addEventListener('click', function () {
-    window.location.href = '../login.html';
-});
-  
-registerBtn.addEventListener('click', function () {
-    window.location.href = '../register.html';
-});
-
-// 카테고리 버튼에 대한 참조를 저장합니다.
-var categoryBtn = document.getElementById('categoryButton');
-
-// 숨겨진 카테고리 요소에 대한 참조를 저장합니다.
-var hiddenCategory = document.getElementById("hiddenCategory");
-
-// 숨겨진 카테고리를 보여주는 함수를 정의합니다.
-function toggleHiddenCategory() {
-    if (hiddenCategory.style.display === "none") {
-        hiddenCategory.style.display = "block";
-    } else {
-        hiddenCategory.style.display = "none";
-    }
-}
-
-// 초기에 카테고리 버튼에 이벤트 리스너를 추가합니다.
-categoryBtn.addEventListener("click", toggleHiddenCategory);
-
-// mypageButton 요소 가져오기
-const mypageButton = document.getElementById('mypageButton');
-
-// mypageButton에 클릭 이벤트 리스너 추가하기
-mypageButton.addEventListener('click', function () {
-     // 사용자가 로그인되었는지 확인합니다. (사용자가 로그인되었다면 여기서 직접 구현할 수 있습니다)
-    const isLoggedIn = false; // 사용자가 로그인되었을 경우 true로 변경합니다.
-
-    // 사용자가 로그인되지 않았다면 login.html 페이지로 이동합니다.
-    if (!isLoggedIn) {
-        window.location.href = '../login.html';
-    } else {
-        // 사용자가 로그인되었으면, 카테고리 버튼의 이벤트 리스너를 제거합니다.
-        categoryBtn.removeEventListener("click", toggleHiddenCategory);
-    }
-});
+//
+// function search() {
+//     var searchText = document.getElementById("searchInput").value;
+//     // 여기에 검색 기능을 구현하는 코드를 작성합니다.
+//     // 검색할 단어(searchText)를 활용하여 검색 동작을 수행합니다.
+//     // 예를 들어, 검색 결과를 보여주거나 검색 페이지로 이동하는 등의 동작을 수행할 수 있습니다.
+//     // 이 예제에서는 검색어를 콘솔에 출력하는 것으로 대체합니다.
+//     console.log("검색어: " + searchText);
+// }
+//
+//
+//
+// var homeBtn = document.getElementById('homeButton');
+// var loginBtn = document.getElementById('loginButton');
+// var registerBtn = document.getElementById('registerButton');
+//
+// homeBtn.addEventListener('click', function () {
+//     window.location.href = 'mainpage.html';
+// });
+//
+// loginBtn.addEventListener('click', function () {
+//     window.location.href = '../login.html';
+// });
+//
+// registerBtn.addEventListener('click', function () {
+//     window.location.href = '../register.html';
+// });
+//
+// // 카테고리 버튼에 대한 참조를 저장합니다.
+// var categoryBtn = document.getElementById('categoryButton');
+//
+// // 숨겨진 카테고리 요소에 대한 참조를 저장합니다.
+// var hiddenCategory = document.getElementById("hiddenCategory");
+//
+// // 숨겨진 카테고리를 보여주는 함수를 정의합니다.
+// function toggleHiddenCategory() {
+//     if (hiddenCategory.style.display === "none") {
+//         hiddenCategory.style.display = "block";
+//     } else {
+//         hiddenCategory.style.display = "none";
+//     }
+// }
+//
+// // 초기에 카테고리 버튼에 이벤트 리스너를 추가합니다.
+// categoryBtn.addEventListener("click", toggleHiddenCategory);
+//
+// // mypageButton 요소 가져오기
+// const mypageButton = document.getElementById('mypageButton');
+//
+// // mypageButton에 클릭 이벤트 리스너 추가하기
+// mypageButton.addEventListener('click', function () {
+//      // 사용자가 로그인되었는지 확인합니다. (사용자가 로그인되었다면 여기서 직접 구현할 수 있습니다)
+//     const isLoggedIn = false; // 사용자가 로그인되었을 경우 true로 변경합니다.
+//
+//     // 사용자가 로그인되지 않았다면 login.html 페이지로 이동합니다.
+//     if (!isLoggedIn) {
+//         window.location.href = '../login.html';
+//     } else {
+//         // 사용자가 로그인되었으면, 카테고리 버튼의 이벤트 리스너를 제거합니다.
+//         categoryBtn.removeEventListener("click", toggleHiddenCategory);
+//     }
+// });
 
 
 // 음식 1//
